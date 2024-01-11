2647ebc116908051876e5c6a8f65f976ef47822e
Mostafa Zeynali <121800106+mzeynali-ir@users.noreply.github.com>
2024-01-11 17:13:26 +0330

    sorted codes

--- HttpClientPlus/HttpClientPlus.cs
+++ HttpClientPlus/HttpClientPlus.cs
@@ -5,13 +5,6 @@ using System.Threading.Tasks;
 
 namespace IMustafa
 {
-    public class HttpClientPlusConfig
-    {
-        public virtual Task BeforeExecuteAsync() => Task.CompletedTask;
-        public virtual Task AfterExecuteAsync(HttpResponseMessage response) => Task.CompletedTask;
-        public virtual Task WhenHasErrorAsync() => Task.CompletedTask;
-        public virtual Task FinalAsync() => Task.CompletedTask;
-    }
 
     public class HttpClientPlus : IDisposable
     {
@@ -102,6 +95,7 @@ namespace IMustafa
                 return _httpClient.PatchAsync(requestUri, content);
             });
         }
+
 
         public void Dispose()
         {
---
+++ HttpClientPlus/HttpClientPlusConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace IMustafa
+{
+    public class HttpClientPlusConfig
+    {
+        public virtual Task BeforeExecuteAsync() => Task.CompletedTask;
+        public virtual Task AfterExecuteAsync(HttpResponseMessage response) => Task.CompletedTask;
+        public virtual Task WhenHasErrorAsync() => Task.CompletedTask;
+        public virtual Task FinalAsync() => Task.CompletedTask;
+    }
+}
+
