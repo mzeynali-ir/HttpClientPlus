1bacef3e2221076c932a0823aefabd53325b6f8f
Mostafa Zeynali <121800106+mzeynali-ir@users.noreply.github.com>
2024-01-11 20:32:11 +0330

    feat: Set Authorization in header basic authorization and bearer authorization

---
+++ HttpClientPlus/Methods/AddBasicAuthentication.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IMustafa
+{
+    public partial class HttpClientPlus
+    {
+        public void AddBasicAuthentication(string base64EncodedAuthenticationString)
+        {
+            string key = "Authorization";
+            this.setHeaders(key, $"Basic {base64EncodedAuthenticationString}");
+        }
+
+        public void AddBasicAuthentication(string clientId, string clientSecret)
+        {
+            var authenticationString = $"{clientId}:{clientSecret}";
+            var base64EncodedAuthenticationString = Convert.ToBase64String(System.Text.ASCIIEncoding.UTF8.GetBytes(authenticationString));
+            string key = "Authorization";
+            this.setHeaders(key, $"Basic {base64EncodedAuthenticationString}");
+        }
+    }
+}
---
+++ HttpClientPlus/Methods/AddBearerAuthentication.cs
@@ -0,0 +1,11 @@
+﻿namespace IMustafa
+{
+    public partial class HttpClientPlus
+    {
+        public void AddBearerAuthentication(string token)
+        {
+            string key = "Authorization";
+            this.setHeaders(key, $"Bearer {token}");
+        }
+    }
+}
